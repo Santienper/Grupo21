a85fff25cbf3b3fc81b94a537990f28ef3e1a722
JaitorWUT <jfelip@ucm.es>
2022-03-21 12:24:24 +0100

    Conflicto 2 B

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Practica_4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hola Mundo");
         }
     }
 }
